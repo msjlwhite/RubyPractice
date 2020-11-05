77651dcbdcb1f7146f281905e74ed6c0dc59deac
Jo Lee White <jw072727@cerner.net>
2020-11-05 09:08:38 -0600

    old practice code?

---
+++ string_Practice.rb
@@ -0,0 +1,10 @@
+=begin
+    2.5.1 Exercises
+Write the Ruby code to test whether the string “hoNeY BaDGer” includes the string “badger” without regard to case.
+Using the documentation, figure out how to capitalize a string. What happens if you capitalize a string that is already all-caps?
+See if you can guess the Ruby boolean method to test if an object is nil (equivalent to object == nil). Use it to show that the empty string isn’t nil.
+
+2.6.1 Exercises
+Write a for loop that prints out the characters of soliloquy in reverse order. Hint: Inside the loop, what is soliloquy.length - i?
+
+end
